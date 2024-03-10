22fcdfb9ce4a68b8dd6d50187b6d2536a896c71d
Kristina Kibalnichenko <kristina_kibalnichenko@epam.com>
2024-03-10 19:28:08 +0100

    HW-3 Task2

---
+++ task2_runner.js
@@ -0,0 +1,10 @@
+import { WithTime, fetchFromUrl } from "./task2.js";
+
+const withTime = new WithTime();
+
+withTime.on('begin', () => console.log('About to execute'));
+withTime.on('end', () => console.log('Done with execute'));
+
+withTime.execute(fetchFromUrl, 'https://jsonplaceholder.typicode.com/posts/1');
+
+console.log(withTime.rawListeners('end'));
