cd20ec7767608d28382003f9c5f7f35c6d4f80c7
Kristina Kibalnichenko <kristina_kibalnichenko@epam.com>
2024-05-12 15:39:28 +0200

    Implement graceful shutdown

--- data-source.ts
+++ data-source.ts
@@ -4,10 +4,12 @@ import * as dotenv from 'dotenv';
 
 dotenv.config({ path: path.join(__dirname, '/.env') });
 
+type db_Type = 'postgres';
+
 export const AppDataSource = new DataSource({
-    type: process.env.DB_TYPE,
+    type: ( process.env.DB_TYPE || 'postgres' ) as db_Type,
     host: process.env.DB_HOST || 'localhost',
-    port: process.env.DB_PORT,
+    port: process.env.DB_PORT as unknown as number,
     username: process.env.DB_USER || 'root',
     password: process.env.DB_PASSWORD,
     database: process.env.DB_NAME,
--- server.ts
+++ server.ts
@@ -3,6 +3,8 @@ import path from 'path';
 import bodyParser from 'body-parser';
 import * as dotenv from 'dotenv';
 import 'reflect-metadata';
+import { Server } from 'http';
+import { Socket } from 'node:net';
 
 import { router as productsRouter } from './controllers/products.controller';
 import { router as cartRouter } from './controllers/cart.controller';
@@ -13,6 +15,7 @@ import { AppDataSource } from './data-source';
 dotenv.config({ path: path.join(__dirname, '/.env') });
 const port = process.env.PORT || 8000;
 const app: Express = express();
+let server: Server;
 
 app.use(bodyParser.urlencoded({extended: false}));
 app.use(bodyParser.json());
@@ -30,7 +33,37 @@ app.use('/api/auth', authRouter);
 (async () => {
     await AppDataSource.initialize().then(() => {
         console.log('Successfully connected to PostgreSQL');
-        app.listen(port, () => console.log(`Listening at http://localhost:${port}`));
+        server = app.listen(port, () => console.log(`Listening at http://localhost:${port}`));
+        let connections: Socket[] = [];
+        server.on('connection', (connection: Socket) => {
+            connections.push(connection);
+            connection.on('close', () => {
+                connections = connections.filter((currentConnection) => currentConnection !== connection);
+            });
+        });
+
+        function gracefulShutdown() {
+            console.log('Received kill signal, shutting down gracefully');
+
+            server.close(() => {
+                console.log('Closed out remaining connections');
+                process.exit(0);
+            });
+
+            setTimeout(() => {
+                console.error('Could not close connections in time, forcefully shutting down');
+                process.exit(1);
+            }, 10000);
+
+            connections.forEach((connection) => connection.end());
+
+            setTimeout(() => {
+                connections.forEach((connection) => connection.destroy());
+            }, 5000);
+        }
+
+        process.on('SIGTERM', gracefulShutdown);
+        process.on('SIGINT', gracefulShutdown);
     }).catch((error: Error) => {
         console.log(`Error connecting to PostgreSQL: ${error}`);
     });
